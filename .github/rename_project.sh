97995b0ff1cbbab49fd72d9fa49c98895b74e76c
racampos <racampos@users.noreply.github.com>
2021-10-05 17:26:53 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="racampos"
+original_name="fastapi_template"
+original_urlname="fastapi-template"
+original_description="Awesome fastapi_template created by racampos"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv fastapi_template $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
